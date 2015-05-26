a768e86c3904bab7fc8852aed45694df698329ed
pschloss <pschloss@umich.edu>
2015-05-26 12:19:46 -0400

    Initial commit

---
+++ code/parallel_make.bash
@@ -0,0 +1,21 @@
+MAIN_TARGET=$1
+LINES=$2
+
+> $MAIN_TARGET.temp
+TARGETS=$(make print-$MAIN_TARGET | sed 's/HE_AN_LIST=//g')
+for T in $TARGETS
+do
+    echo make $T >> $MAIN_TARGET.temp
+done
+
+split -l $LINES $MAIN_TARGET.temp
+
+for X in x??
+do
+    cat head.batch $X tail.batch > $X.qsub
+    qsub $X.qsub
+    rm $X.qsub $X
+done
+
+rm $MAIN_TARGET.temp
+
