340519e60b6641171d0fe05307989458ce9c48cf
pschloss <pschloss@umich.edu>
2015-05-28 13:57:34 -0400

    Add garbage collection

--- code/run_he_cluster.sh
+++ code/run_he_cluster.sh
@@ -8,3 +8,6 @@ mothur "#set.dir(input=data/he, output=data/he);
         cluster(column=current, name=current, method=average);
         cluster(column=current, name=current, method=furthest);
         cluster(column=current, name=current, method=nearest);"
+
+S_R_ABUND=$(echo $FASTA | sed 's/fasta/*abund/')
+rm $S_R_ABUND
