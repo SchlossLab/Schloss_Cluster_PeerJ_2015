b661ee9b0434fcfbae85d9e1f257eac954744f67
pschloss <pschloss@umich.edu>
2015-05-28 13:02:17 -0400

    Develop script to convert shared file to list file

---
+++ code/shared_to_list.R
@@ -0,0 +1,23 @@
+shared_to_list <- function(shared_file_name){
+
+	shared_file <- read.table(shared_file_name, header=TRUE, row.names=2)
+	shared <- shared_file[,-c(1,2)]
+
+	rabund <- apply(shared, 2, sum)
+	o <- order(rabund, decreasing=TRUE)
+	shared <- shared[,o]
+
+	get_composition <- function(column){
+		paste(rownames(shared)[which(shared[,column] > 0)], collapse=",")
+	}
+
+	n_otus <- ncol(shared)
+	list_data <- paste(unlist(lapply(1:n_otus, get_composition)), collapse="\t")
+	list_line <- paste("userLabel", n_otus, list_data, sep="\t")
+
+
+	list_file_name <- gsub("/otu_table.shared", ".list", shared_file_name)
+	write(list_line, list_file_name)
+
+}
+
