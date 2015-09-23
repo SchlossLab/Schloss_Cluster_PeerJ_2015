ba2d00e1b6786f102bfb9517ead03d63b29a61f6
pschloss <pschloss@umich.edu>
2015-09-23 10:16:57 -0400

    Rename vsearch files to vdgc to reflect default parameters

--- code/run_vdgc_clust.sh
+++ code/run_vdgc_clust.sh
@@ -7,7 +7,7 @@ code/vsearch/vsearch --sizeout --derep_fulllength $FASTA --minseqlength 30 --thr
 
 code/vsearch/vsearch --maxaccepts 16 --usersort --id 0.97 --minseqlength 30 --wordlength 8 --uc $ROOT.clustered.uc --cluster_smallmem $ROOT.sorted.fna --maxrejects 64 --strand both --log $ROOT.clustered.log
 
-R -e "source('code/uc_to_list.R'); uc_to_list('$ROOT.sorted.uc', '$ROOT.clustered.uc', 'vdgc')"
+R -e "source('code/uc_to_list.R'); uc_to_list('$ROOT.sorted.uc', '$ROOT.clustered.uc')"
 
 rm $ROOT.sorted.uc $ROOT.sorted.fna $ROOT.sorted.log $ROOT.clustered.uc $ROOT.clustered.log
 
