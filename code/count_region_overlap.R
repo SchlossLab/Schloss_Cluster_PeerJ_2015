84b8c41bfcab639daf0417b3bf24966077b16ec4
pschloss <pschloss@umich.edu>
2015-10-12 13:53:47 -0400

    Add number of full length sequences

--- code/count_region_overlap.R
+++ code/count_region_overlap.R
@@ -1,5 +1,6 @@
 gg_summary <- read.table(file="data/gg_13_8/gg_13_8_97.v19.summary", header=T)
 n_v4_overlap <- sum(gg_summary$start <= 3967 & gg_summary$end >= 6116)
 n_v9_overlap <- sum(gg_summary$start <= 10179 & gg_summary$end >= 10683)
+n_v19_total <- nrow(gg_summary)
 
-write.table(file="data/gg_13_8/gg_13_8_97.overlap.count", c(v4=n_v4_overlap, v9=n_v9_overlap), col.names=F, quote=F)
+write.table(file="data/gg_13_8/gg_13_8_97.overlap.count", c(v4=n_v4_overlap, v9=n_v9_overlap, v19=n_v19_total), col.names=F, quote=F)
