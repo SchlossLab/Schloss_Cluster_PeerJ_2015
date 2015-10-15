fe23385895758751d644b9a2859896c2b417fbaa
pschloss <pschloss@umich.edu>
2015-10-15 12:42:24 -0400

    Change number of processors to 4

--- code/map_mouse_to_gg.sh
+++ code/map_mouse_to_gg.sh
@@ -9,7 +9,7 @@
 # Output:
 #	data/rand_ref/miseq.ref.mapping
 
-mothur "#align.seqs(fasta=data/miseq/miseq.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.fasta, reference=data/references/silva.bact_archaea.align, outputdir=data/rand_ref, processors=4); screen.seqs(fasta=data/gg_13_8/97_otus.align, start=13862, end=23443, outputdir=data/rand_ref, processors=8); filter.seqs(fasta=data/rand_ref/miseq.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.align-data/rand_ref/97_otus.good.align, vertical=T, trump=., processors=8);"
+mothur "#align.seqs(fasta=data/miseq/miseq.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.fasta, reference=data/references/silva.bact_archaea.align, outputdir=data/rand_ref, processors=4); screen.seqs(fasta=data/gg_13_8/97_otus.align, start=13862, end=23443, outputdir=data/rand_ref, processors=4); filter.seqs(fasta=data/rand_ref/miseq.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.align-data/rand_ref/97_otus.good.align, vertical=T, trump=., processors=4);"
 
 R -e "source('code/map_to_reference.R')"
 
