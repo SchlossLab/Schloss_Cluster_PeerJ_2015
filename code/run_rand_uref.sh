fdf70e2e140ed048a8c13b8d2596d82fa9ad8cc2
pschloss <pschloss@umich.edu>
2015-10-16 14:21:59 -0400

    Make run_rand_?ref.sh scripts run the same way

--- code/run_rand_uref.sh
+++ code/run_rand_uref.sh
@@ -2,9 +2,12 @@ REF=$1
 FASTA=data/rand_ref/miseq.fasta
 CLOSED_PATH=$(echo $REF | sed 's/fasta/uclosed/')
 rm -rf $CLOSED_PATH/
+mkdir $CLOSED_PATH
+mkdir $CLOSED_PATH/usearch
 
-pick_closed_reference_otus.py -i $FASTA -r $REF -o $CLOSED_PATH -p code/closedref.params.txt
-mv $CLOSED_PATH/usearch61_ref_picked_otus/ref_clustered.uc $CLOSED_PATH.uc
+usearch61 --sizeout --derep_fulllength $FASTA --minseqlength 30 --threads 1 --uc $CLOSED_PATH/usearch/abundance_sorted.uc --output $CLOSED_PATH/usearch/abundance_sorted.fna --strand both --log $CLOSED_PATH/usearch/abundance_sorted.log 
+usearch61 --maxaccepts 16 --id 0.97 --minseqlength 30 --threads 1 --wordlength 8 --uc $CLOSED_PATH/usearch/ref_clustered.uc --maxrejects 64 --strand both --db $REF --log $CLOSED_PATH/usearch/ref_clustered.log --usearch_global $CLOSED_PATH/usearch/abundance_sorted.fna
 
+mv $CLOSED_PATH/usearch/ref_clustered.uc $CLOSED_PATH.uc
 rm -rf $CLOSED_PATH/
 
--- code/run_rand_vref.sh
+++ code/run_rand_vref.sh
@@ -6,7 +6,7 @@ mkdir $CLOSED_PATH
 mkdir $CLOSED_PATH/vsearch
 
 code/vsearch/vsearch --sizeout --derep_fulllength $FASTA --minseqlength 30 --threads 1 --uc $CLOSED_PATH/vsearch/abundance_sorted.uc --output $CLOSED_PATH/vsearch/abundance_sorted.fna --strand both --log $CLOSED_PATH/vsearch/abundance_sorted.log 
-code/vsearch/vsearch --maxaccepts 16 --id 0.97 --minseqlength 30 --threads 1 --wordlength 8 --uc $CLOSED_PATH/vsearch/ref_clustered.uc --maxrejects 64 --strand both --db $REF --log $CLOSED_PATH/vsearch/ref_clustered.log --usearch_global $CLOSED_PATH/vsearch/abundance_sorted.fna 
+code/vsearch/vsearch --maxaccepts 16 --id 0.97 --minseqlength 30 --threads 1 --wordlength 8 --uc $CLOSED_PATH/vsearch/ref_clustered.uc --maxrejects 64 --strand both --db $REF --log $CLOSED_PATH/vsearch/ref_clustered.log --usearch_global $CLOSED_PATH/vsearch/abundance_sorted.fna
 
 mv $CLOSED_PATH/vsearch/ref_clustered.uc $CLOSED_PATH.vc
 rm -rf $CLOSED_PATH/
