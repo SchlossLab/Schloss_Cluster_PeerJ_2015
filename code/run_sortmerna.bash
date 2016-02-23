ed4568c54644e8263e4a29ffec6fa4854828e033
Pat Schloss <pschloss@umich.edu>
2016-02-23 16:58:53 -0500

    Add sortmerna commands

---
+++ code/run_sortmerna.bash
@@ -0,0 +1,16 @@
+# This script implements the sortmerna algorithm as implemented in qiime.
+# Because the output is weird we change it into a mothur list file. The input
+# is a degapped and redundant fasta file and the output is a list file where
+# sortmerna is used as the method tag. We'll assume that QIIME is in the PATH.
+# We will also assign sequences to OTUs based on 97% similarity (default). The
+# peculiarity of the output file is that the reference is the first column and
+# the matching sequences are in subsequent columns.
+
+FASTA=$1
+SORTMERNA=$(echo $FASTA | sed 's/fasta/sortmerna/')
+
+parallel_pick_otus_sortmerna.py -i $FASTA -o $SORTMERNA -r data/references/97_otus.fasta -T --jobs_to_start 1 --threads 10 --sortmerna_db data/references/97_otus.idx
+
+R -e "source('code/sortmerna_to_list.R'); sortmerna_to_list('$SORTMERNA')"
+
+rm -rf $SORTMERNA
---
+++ code/sortmerna_to_list.R
@@ -0,0 +1,15 @@
+# the sortmrna format puts otus on separate rows and the sequences within each
+# otu on the same line separated by tabs. the reference sequence name is in the
+# first column and can be ignored.
+
+sortmerna_to_list <- function(sortmerna_folder_name){
+	sortmerna_file <- paste0(sortmerna_folder_name, "/seqs_otus.txt")
+	sortmerna_data <- scan(sortmerna_file, what="", sep="\n", quiet=TRUE)
+	sortmerna_data <- gsub("^[^\t]*\t", "", sortmerna_data)
+	sortmerna_data <- gsub('\t', ',', sortmerna_data)
+	n_otus <- length(sortmerna_data)
+	list_data <- paste(c("userLabel", n_otus, sortmerna_data), collapse='\t')
+	list_file_name <- paste0(sortmerna_folder_name, ".list")
+	list_file_name <- gsub("\\.ng", "", list_file_name)
+	write(list_data, list_file_name)
+}
