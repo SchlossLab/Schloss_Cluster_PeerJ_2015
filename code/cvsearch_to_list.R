3dd3ad160caa2aef3a60d21aeec035558fe22a3b
Pat Schloss <pschloss@umich.edu>
2016-02-25 12:42:10 -0500

    Write scripts to process closed-reference vsearch clustering

--- code/cvsearch_to_list.R
+++ code/cvsearch_to_list.R
@@ -1,11 +1,38 @@
-form_otu <- function(line){
-	unlist(strplit(line, "\t"))[-1]
-}
-
-cvsearch_to_list <- function(clustered_file_name){
+cvsearch_to_list <- function(unique_file_name, clustered_file_name){
+	unique_file_name <- "data/he/he_1.0_01.vclosed/vsearch/abundance_sorted.uc"
 	clustered_file_name <- "data/he/he_1.0_01.vclosed/vsearch/ref_clustered.uc"
-	clustered_data <- scan(clustered_file_name, what=character(), sep='\n', quiet=T)
 
+	uniqued <- read.table(file=unique_file_name, stringsAsFactors=FALSE)
 
+	names_first_column <- uniqued[uniqued$V1=="S", "V9"]
+	names_second_column <- names_first_column
 
+	hits <- uniqued[uniqued$V1=="H", ]
+
+	for(i in 0:(length(names_first_column)-1)){
+		dups <- paste(hits[hits$V2==i, "V9"], collapse=",")
+		names_second_column[i+1] <- paste(names_second_column[i+1], dups, sep=",")
+	}
+	names_second_column <- gsub(",$", "", names_second_column)
+	names(names_second_column) <- names_first_column
+
+	clustered <- read.table(file=clustered_file_name, stringsAsFactors=FALSE)
+	unique_sequence <- gsub(";size=.*", "", clustered$V9)
+	duplicate_sequences <- names_second_column[unique_sequence]
+
+	ref_sequence <- as.character(clustered$V10)
+
+	otu_list <- character()
+
+	for(i in 1:length(ref_sequence)){
+		if(is.na(otu_list[ref_sequence[i]])){
+			otu_list[ref_sequence[i]] <- duplicate_sequences[i]
+		} else {
+			otu_list[ref_sequence[i]] <- paste(otu_list[ref_sequence[i]], duplicate_sequences[i], sep=',')
+		}
+	}
+
+	list_file_data <- paste(c("userLabel", length(otu_list), otu_list), collapse='\t')
+	list_file_name <- gsub("/vsearch/abundance_sorted.uc", ".list", unique_file_name)
+	write(list_file_data, list_file_name)
 }
--- code/run_cvsearch.sh
+++ code/run_cvsearch.sh
@@ -10,6 +10,9 @@ mkdir $CLOSED_PATH/vsearch
 code/vsearch/vsearch --sizeout --derep_fulllength $FASTA --minseqlength 30 --threads 1 --uc $CLOSED_PATH/vsearch/abundance_sorted.uc --output $CLOSED_PATH/vsearch/abundance_sorted.fna --strand both --log $CLOSED_PATH/vsearch/abundance_sorted.log
 code/vsearch/vsearch --maxaccepts 16 --id 0.97 --minseqlength 30 --threads 1 --wordlength 8 --uc $CLOSED_PATH/vsearch/ref_clustered.uc --maxrejects 64 --strand both --db $REF --log $CLOSED_PATH/vsearch/ref_clustered.log --usearch_global $CLOSED_PATH/vsearch/abundance_sorted.fna
 
-R -e "source(code/cvsearch_to_list.R); cvsearch_to_list.R('$CLOSED_PATH/vsearch/ref_clustered.uc')"
+	unique_file_name <- "data/he/he_1.0_01.vclosed/vsearch/abundance_sorted.uc"
+	clustered_file_name <- "data/he/he_1.0_01.vclosed/vsearch/ref_clustered.uc"
+
+R -e "source(code/cvsearch_to_list.R); cvsearch_to_list.R('$CLOSED_PATH/vsearch/abundance_sorted.uc', '$CLOSED_PATH/vsearch/ref_clustered.uc')"
 
 rm -rf $CLOSED_PATH/
