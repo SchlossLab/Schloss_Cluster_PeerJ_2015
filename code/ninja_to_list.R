07bc9edc780bd8e6ae8e1dfb279aa2091c64db15
pschloss <pschloss@umich.edu>
2016-02-26 16:14:43 -0500

    Remove typos to get NINJA-OPS rules running

--- code/ninja_to_list.R
+++ code/ninja_to_list.R
@@ -2,11 +2,10 @@
 # first column is the sequence name and the second column is the references
 # sequence that it mapped to. We need to convert this to a list file...
 
-library(dplyr)
+library(dplyr, warn.conflicts=F, quietly=T)
 
 ninja_to_list <- function(folder_name){
 
-	folder_name <- "even_1.0_01.ng.ninja"
 	input_file_name <- paste0(folder_name, '/ninja_pass.log')
 	output_file_name <- paste0(folder_name, '.list')
 	output_file_name <- gsub("ng.", "", output_file_name)
@@ -16,7 +15,7 @@ ninja_to_list <- function(folder_name){
 
 	list_data <- mapping %>% group_by(factor(V2)) %>% summarise(otu=paste(V1, collapse=','))
 
-	list_output <- paste(c("userLabel", length(list_data), list_data), collapse='\t')
+	list_output <- paste(c("userLabel", length(list_data$otu), list_data$otu), collapse='\t')
 
 	write(list_output, output_file_name)
 
